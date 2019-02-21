5ef169d7bd9795330c410ba207f809b89598589a
Heroi211 <gabriel_s4@hotmail.com>
2019-02-21 17:37:57 -0300

    Criando o projeto, criando a tela de login e o metodo post

--- Controle/Controllers/HomeController.cs
+++ Controle/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Controle.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,17 @@ namespace Controle.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
-            return View();
+            if (Session["usuarioLogadoID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
         }
 
         public ActionResult About()
@@ -25,6 +34,32 @@ namespace Controle.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Usuario u)
+        {
+            // esta action trata o post (login)
+            if (ModelState.IsValid) //verifica se é válido
+            {
+                using (ControleEntities dc = new ControleEntities())
+                {
+                    var v = dc.Usuario.Where(a => a.Email.Equals(u.Email) && a.Senha.Equals(u.Senha)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["usuarioLogadoID"] = v.Id.ToString();
+                        Session["Email"] = v.Email.ToString();
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            
+            return View(u);
         }
     }
 }
